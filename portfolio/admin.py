969b91635e22f003e87fab65a66ae6c73eba6dbc
InquisitorFury <studentaccahmed@gmail.com>
2023-02-21 20:29:13 +0000

    projects in adminsite
    the projects db are now added to the adminsite, where the admin could easily add projects, edit them to fit their needs.

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Projects
 # Register your models here.
+
+# this will add the projects class into the admin site
+# for the admin to change it
+admin.site.register(Projects) 
