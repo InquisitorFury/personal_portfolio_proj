3dcc71a343cf171d6f64e0bfaa53e9af12696864
InquisitorFury <studentaccahmed@gmail.com>
2023-02-21 20:48:55 +0000

    media folder in base dir created
    created the media folder in the base dir so that it can be accessed by django when user presses on any media files

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -113,6 +113,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_URL =  "media/"
+MEDIA_ROOT = BASE_DIR / "media" # place to save all media files [videos, images] from all apps 
+#adds media folder to the base dir 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
