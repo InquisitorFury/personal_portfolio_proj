dfd2cad98db287c84876ad0a8fd37efbd67a5553
InquisitorFury <studentaccahmed@gmail.com>
2023-02-22 12:31:52 +0000

    blog app and blog db created
    blog app and it's db have been constructed. The db is implemented into the html file and it's displayed. The blog has it's own url file.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class blog(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    date = models.DateField(auto_now=False)
+    images = models.ImageField(upload_to='blogs/images/')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import blog
 # Create your views here.
+def all_blogs(request):
+    articles = blog.objects.all()
+    return render(request, 'blog/all_blogs.html', {'articles':articles})
